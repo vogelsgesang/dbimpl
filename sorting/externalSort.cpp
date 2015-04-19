88443088380608224610fe03a05a42818160f68c
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-04-19 19:09:41 +0200

    sort: preparations for the implementation of the merge phase
    * informations about runs are stored in a list of runDescriptors
    * the merged run will be carved out of the temporary file after merging
    * memory allocations are checked for errors

--- sorting/externalSort.cpp
+++ sorting/externalSort.cpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <memory>
 #include <algorithm>
+#include <vector>
+#include <queue>
 
 #include "externalSort.h"
 
@@ -19,13 +21,24 @@ void checkedPread(int fd, void* buf, size_t count, off_t offset);
 //if an error occurs exit() will be called.
 void checkedPwrite(int fd, void* buf, size_t count, off_t offset);
 
+//holds informations about a run of sorted numbers
+struct runDescriptor {
+  off_t offset; //offset from start of file in bytes
+  size_t size; //lenght of the run in number of elements
+};
+typedef std::vector<runDescriptor> runList;
+
+//merges runs of sorted numbers
+//returns the descriptor for the resulting run
+runDescriptor mergeRuns(const runList& runs, int fd, uint64_t runsMergedPerSweep);
+
 void externalSort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
   /*
    * implementation decisions:
-   *  * std::vector is avoided, since std::vector initializes its memory with 0 and
-   *    this initialization would be useless overhead
+   *  * std::vector is avoided for holding the uint64_t elements since std::vector
+   *    initializes its memory with 0 and this initialization would be useless overhead
    *  * all file accesses are done using the preferred block size as reported by fstat
-   *  * posix_fadvise is used to give hints for tuning
+   *  * posix_fadvise is used to give hints for tuning disk accesses
    */
   //calculate block sizes, nr. of blocks, ...
   struct stat inStat;
@@ -49,35 +62,73 @@ void externalSort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
   std::clog << "number of blocks kept in memory: " << blocksInMemory << std::endl;
   std::clog << "used memory: " << usedMemory << " bytes" << std::endl;
   #endif
+  if(blocksInMemory < 3) {
+    std::cerr << "unable to merge runs: not enough memory" << std::endl;
+    exit(1);
+  }
   //form sorted runs
+  runList runs;
   {
     #ifdef DEBUG
     std::clog << "forming runs..." << std::endl;
     #endif
-    posix_fadvise(fdInput, 0, size*sizeof(uint64_t), POSIX_FADV_SEQUENTIAL);
     if(int ret = posix_fallocate(fdOutput, 0, size * sizeof(uint64_t))) {
       std::cerr << "unable to allocate disk space: " << strerror(ret);
       exit(1);
     }
-    std::unique_ptr<uint64_t[]> run(new uint64_t[usedMemory / sizeof(uint64_t)]);
+    posix_fadvise(fdInput, 0, size*sizeof(uint64_t), POSIX_FADV_SEQUENTIAL);
+    std::unique_ptr<uint64_t[]> run(new (std::nothrow) uint64_t[usedMemory / sizeof(uint64_t)]);
+    if(!run) {
+      std::cerr << "unable to allocate buffer" << std::endl;
+      exit(1);
+    }
     uint64_t elementsProcessed = 0;
     while(elementsProcessed < size) {
-      uint64_t runSize = std::min(size - elementsProcessed, usedMemory / sizeof(uint64_t));
-      uint64_t offset = elementsProcessed * sizeof(uint64_t);
-      uint64_t runBytes = runSize * sizeof(uint64_t);
-      #ifdef DEBUG
-      std::clog << "offset: " << offset << " bytes, len: " << runSize << " elements" << std::endl;
-      #endif
+      size_t runSize = std::min(size - elementsProcessed, usedMemory / sizeof(uint64_t));
+      off_t offset = elementsProcessed * sizeof(uint64_t);
+      size_t runBytes = runSize * sizeof(uint64_t);
       checkedPread(fdInput, &run[0], runBytes, offset);
       std::sort(&run[0], &run[runSize]);
       checkedPwrite(fdOutput, &run[0], runBytes, offset);
       posix_fadvise(fdInput, offset, runBytes, POSIX_FADV_DONTNEED);
+      runs.push_back({.offset = offset, .size = runSize});
       elementsProcessed += runSize;
     }
   } //variables will go out of scope and the memory will be freed
 
   //merge runs
-  //write result to fdOutput
+  #ifdef DEBUG
+  std::clog << "merging runs..." << std::endl;
+  #endif
+  uint64_t runsMergedPerSweep = blocksInMemory-1;
+  runDescriptor mergedRun = mergeRuns(runs, fdOutput, runsMergedPerSweep);
+
+  //truncate the output file so it only contains the mergedRun
+  #ifdef DEBUG
+  std::clog << "carving sorted data out of temporary files..." << std::endl;
+  #endif
+  if(mergedRun.offset != 0) {
+    //move the data to the beginning of the file.
+    //fallocate with FALLOC_FL_COLLAPSE_RANGE would be a better solution
+    //but it is only supported on ext4 and xfs file systems.
+    size_t runBytes = mergedRun.size*sizeof(uint64_t);
+    off_t offset = 0;
+    std::unique_ptr<uint8_t[]> buffer(new (std::nothrow) uint8_t[usedMemory]);
+    if(!buffer) {
+      std::cerr << "unable to allocate buffer" << std::endl;
+      exit(1);
+    }
+    while(static_cast<size_t>(offset) < runBytes) {
+      size_t chunkSize = std::min(runBytes - offset, usedMemory);
+      checkedPread(fdOutput, &buffer[0], chunkSize, mergedRun.offset + offset);
+      checkedPwrite(fdOutput, &buffer[0], chunkSize, offset);
+      offset += chunkSize;
+    }
+  }
+  if(ftruncate(fdOutput, mergedRun.size) != 0) {
+    std::cerr << "unable to truncate output file: " << strerror(errno) << std::endl;
+    exit(1);
+  }
 }
 
 void checkedPread(int fd, void* buf, size_t count, off_t offset) {
@@ -86,7 +137,7 @@ void checkedPread(int fd, void* buf, size_t count, off_t offset) {
     ssize_t ret = pread(fd, (uint8_t*)buf + bytesRead, count - bytesRead, offset + bytesRead);
     if(ret < 0) {
       std::cerr << "I/O error while reading from file descriptor " << fd << ": "
-        << strerror(errno) << std::endl;
+                << strerror(errno) << std::endl;
       exit(1);
     }
     if(ret == 0) {
@@ -104,11 +155,20 @@ void checkedPwrite(int fd, void* buf, size_t count, off_t offset) {
     ssize_t ret = pwrite(fd, (uint8_t*)buf + bytesWritten, count - bytesWritten, offset + bytesWritten);
     if(ret < 0) {
       std::cerr << "I/O error while writing to file descriptor " << fd << ": "
-        << strerror(errno) << std::endl;
+                << strerror(errno) << std::endl;
       exit(1);
     }
     bytesWritten += ret;
   }
 }
 
+runDescriptor mergeRuns(const runList& runs, int fd, uint64_t runsMergedPerSweep) {
+  //necessary for avoiding "unused parameters" compiler warning...
+  int unused = fd + runsMergedPerSweep;
+  runs[unused];
+  //for now, simply return the last run
+  return runs.back();
+  //TODO: implement the merge phase
 }
+
+} //namespace dbImpl
