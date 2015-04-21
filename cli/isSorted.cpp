9b92114d0a070a4628cd70e4d11214d8a1cb77c1
bottey <janboettcher@gmx.net>
2015-04-21 10:26:57 +0200

    Added test isSorted

---
+++ cli/isSorted.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "sorting/externalSort.h"
+
+
+//  bool isSorted(uint64_t size, int fdTest, uint64_t memSize);
+void printHelp();
+
+int main(int argc, const char* argv[]) {
+  if(argc < 3) {
+    std::cerr << "Not enough arguments" << std::endl;
+    printHelp();
+    return 1;
+  } else if (argc > 3) {
+    std::cerr << "Too many arguments" << std::endl;
+    printHelp();
+    return 1;
+  }
+  //parse the memory size
+  char* lastChar;
+  uint64_t memSize = std::strtoull(argv[2], &lastChar, 10);
+  if(*lastChar) {
+    std::cerr << "unable to parse memory size: unexpected character \"" << *lastChar << "\"" << std::endl;
+    return 1;
+  }
+  if(errno == ERANGE) {
+    std::cerr << "unable to parse memory size: " << std::strerror(errno) << std::endl;
+    return 1;
+  }
+  if(memSize <= 0) {
+    std::cerr << "memory size <= 0" << std::endl;
+    return 1;
+  }
+  //avoid overflows
+  if(memSize & (0xfffffL << 44)) {
+    std::cerr << "memory size too big: an overflow would occur" << std::endl;
+    return 1;
+  }
+  memSize <<= 20; //convert Megabyte to Byte
+  #ifdef DEBUG
+  std::clog << "memory size: " << memSize << " Bytes" << std::endl;
+  #endif
+  //open the file to be tested
+  int fdIn = open(argv[1], O_RDONLY);
+  if(fdIn < 0) {
+    std::cerr << "unable to open file '" << argv[1] << "': " << strerror(errno) << std::endl;
+    return 1;
+  }
+
+  //get file size of file to be tested
+  int ret;
+  struct stat inStat;
+  if((ret = fstat(fdIn, &inStat)) != 0) {
+    std::cerr << "unable to stat file '" << argv[1] << "': " << strerror(errno) << std::endl;
+    return 1;
+  }
+  uint64_t nrIntegers = inStat.st_size/sizeof(uint64_t);
+  #ifdef DEBUG
+  std::clog << "size of input file: " << inStat.st_size << " Bytes" << std::endl;
+  std::clog << "number of uint64_t integers: " << nrIntegers << std::endl;
+  #endif
+  //call sort algorithm
+  dbImpl::isSorted(nrIntegers, fdIn, memSize);
+  //close file descriptors
+  if((ret = close(fdIn)) != 0) {
+    std::cerr << "unable to close file '" << argv[1] << "': " << strerror(errno) << std::endl;
+    return 1;
+  }
+  if((ret = close(fdOut)) != 0) {
+    std::cerr << "unable to close file '" << argv[1] << "': " << strerror(errno) << std::endl;
+    return 1;
+  }
+}
+
+void printHelp() {
+  std::cerr << "Usage:" << std::endl
+            << "  isSorted <number of integers> <infile> <memoryBufferInMB>" << std::endl;
+}
---
+++ tests/isSorted.h
@@ -0,0 +1,15 @@
+#ifndef __IS_SORTED_HPP__
+#define __IS_SORTED_HPP__
+
+#include <cstdint> //uint64_t
+
+namespace dbImpl {
+
+  //reads a file consisting out of uint64_t integers from `fdTest` and checks whether the numbers are sorted in ascending order. 
+  //`memSize` are the bytes of available main memory and
+  //`size` is the number of integers.
+  // Returns true iff numbers are sorted properly.
+  bool isSorted(uint64_t size, int fdTest, uint64_t memSize);
+
+}
+#endif
