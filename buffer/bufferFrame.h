bccf7db5afa3554e9af20705ce1433b97c788f91
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-05-03 13:54:14 +0200

    made BufferFrame non copyable

--- buffer/bufferFrame.h
+++ buffer/bufferFrame.h
@@ -7,28 +7,32 @@
 
 namespace dbImpl {
   class BufferFrame {
-  public:
-    uint64_t pageId;
-    // file descriptor for segment file, wherein page is stored
-    int segmentFd;
-    
-    // constructor
-    BufferFrame(uint64_t pageId, int segmentFd);
-    // destructor
-    virtual ~BufferFrame();
-    // returns data from page
-    void* getData();
-    // locks frame with an write or read lock whether lock is exclusive or not
-    void lock(bool exclusive);
-    // unlocks frame
-    void unlock();
-    
-  private:
-    // actual data
-    void* data;
-    // frame's lock
-    pthread_rwlock_t latch;
-    
+    public:
+      //deleted copy constructor => non copyable
+      BufferFrame(const BufferFrame&) = delete;
+      //deleted operator= => non copyable
+      const BufferFrame& operator=(const BufferFrame&) = delete;
+
+      uint64_t pageId;
+      // file descriptor for segment file, wherein page is stored
+      int segmentFd;
+      
+      // constructor
+      BufferFrame(uint64_t pageId, int segmentFd);
+      // destructor
+      virtual ~BufferFrame();
+      // returns data from page
+      void* getData();
+      // locks frame with an write or read lock whether lock is exclusive or not
+      void lock(bool exclusive);
+      // unlocks frame
+      void unlock();
+      
+    private:
+      // actual data
+      void* data;
+      // frame's lock
+      pthread_rwlock_t latch;
   };
 }
 
