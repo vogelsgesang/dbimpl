abcf9ba0181983d713e2d4ed8438630abe32f09b
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-05-17 19:41:42 +0200

    slottedPagesTest: bug fix in test case

--- tests/slottedPagesTest.cpp
+++ tests/slottedPagesTest.cpp
@@ -9,11 +9,11 @@ TEST(SlottedPagesTest, storesRecords) {
   dbImpl::SPSegment spSegment(bm, 1);
   
   std::string testStr("Test");
-  dbImpl::Record testRecord(testStr.length(), reinterpret_cast<const uint8_t*>(testStr.c_str()));
+  dbImpl::Record testRecord(testStr.length() + 1, reinterpret_cast<const uint8_t*>(testStr.c_str()));
   uint64_t tid = spSegment.insert(testRecord);
 
   dbImpl::Record readRecord = spSegment.lookup(tid);
-  EXPECT_EQ(testStr.length(), readRecord.getLen());
+  EXPECT_EQ(testStr.length()+1, readRecord.getLen());
   EXPECT_STREQ(testStr.c_str(), readRecord.getData());
 }
 
