4b864d37d9c8d36780b017b44febf1a0886deb5f
Sepp1314 <janboettcher@gmx.net>
2015-05-22 18:10:40 +0200

    Some helper functions for BTree

--- bTree/bTree.h
+++ bTree/bTree.h
@@ -11,30 +11,42 @@ template<typename K,typename Comp>
 class BTree {
 
 private:
-  struct InnerNode{
+  struct Node{
     uint64_t lsn;
     uint64_t upper; //upper page of right-most child
-    uint64_t count = 0; //number of entries
-    std::pair<K,uint64_t> keyChildPIDPairs[1];
+    uint64_t count(0); //number of entries
+    std::pair<K,uint64_t> keyChildPIDPairs[calculateMaxChildren(false)];
   };
 
   struct Leaf{
      uint64_t lsn;
-     uint_64 isLeaf = ~0; //mark as Leaf
+     uint64_t isLeaf(~0); //mark as Leaf
      Leaf* next; //next leaf node
-     uint64_t count = 0; //number of entries
-     std::pair<K,uint64_t> keyTIDPairs[1];
+     uint64_t count(0); //number of entries
+     std::pair<K,uint64_t> keyTIDPairs[calculateMaxChildren(true)];
   };
 
+  inline uint64_t calculateMaxChildren(bool isLeaf);
+  inline bool Node::isFull();
+  inline bool Node::isLeaf();
+  inline uint64_t Node::findKeyPos(const K key);
+  inline uint64_t Leaf::findKeyPos(const K key);
+
+
+  Node root*;
+  Comparator smaller;
+
 public:
+
   bool insert(K key, uint64_t tid);
   bool erase(K key);
   std::experimental::optional<uint64_t> lookup (K key); //Returns a TID or indicates that the key was not found.
   std::vector<uint64_t>::iterator lookupRange(K key1, K key2);
-
 };
 
 
-
 }
+#include "bTree/bTreeImpl.h"
+
+
 #endif //_B_TREE_H_
