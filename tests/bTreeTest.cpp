bc089a261fc49fe87ac1038687469dadf6000666
bottey <janboettcher@gmx.net>
2015-05-24 10:35:09 +0200

    Added Testcase insertNodeSorted

--- tests/bTreeTest.cpp
+++ tests/bTreeTest.cpp
@@ -17,6 +17,21 @@ TEST(BTreeTest, insertOneNode) {
   test.insert(10, 50);
   ASSERT_TRUE(test.lookup(10));
   EXPECT_EQ(50, test.lookup(10).get());
+  //bm.~BufferManager();
+
+}
+TEST(BTreeTest, insertNodedSorted) {
+  BufferManager bm(50);
+  BTree<uint64_t, UInt64Cmp> test(bm);
+
+  for (int i = 10; i < 20; i++) {
+    test.insert(i, 2 * i);
+  }
+  for (int i = 10; i < 20; i++) {
+    ASSERT_TRUE(test.lookup(i));
+    EXPECT_EQ(i * 2, test.lookup(i).get());
+  }
+  //bm.~BufferManager();
 
 }
 
