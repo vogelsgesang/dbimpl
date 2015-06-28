1dee8c9484171d4f5fb5e75110fe77b013a18c80
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-06-28 15:01:00 +0200

    chainedRangeIterator.h: indentation

--- hashjoin/chainedRangeIterator.h
+++ hashjoin/chainedRangeIterator.h
@@ -1,69 +1,69 @@
 #ifndef _CHAINED_RANGEITERATOR_H_
 #define _CHAINED_RANGEITERATOR_H_
-//this iterator is used to iterate over the results of a lookup
+
+#include <cstdint>
 
 struct Entry {
-      uint64_t key;
-      uint64_t value;
-      Entry* next;
-    };
-class RangeIterator {
-
-public:
-  RangeIterator(Entry* entry, uint64_t key) :
-      entry(entry), key(key) {
-  }
-
-  uint64_t operator*() const {
-    return entry->value;
-  }
-
-  RangeIterator& operator++() {
-    do {
-      entry = entry->next;
-    } while (entry != nullptr && entry->key != key);
-    return *this;
-  }
-
-  RangeIterator operator++(int) {
-    RangeIterator r(*this);
-    operator++();
-    return r;
-  }
-  ;
-
-  bool operator==(const RangeIterator& rhs) const {
-    return entry == rhs.entry && key == rhs.key;
-  }
-
-  bool operator!=(const RangeIterator& rhs) const {
-    return !operator==(rhs);
-  }
-
-private:
-  Entry* entry;
   uint64_t key;
+  uint64_t value;
+  Entry* next;
+};
+
+//this iterator is used to iterate over the results of a lookup
+class RangeIterator {
+  public:
+    RangeIterator(Entry* entry, uint64_t key) :
+      entry(entry), key(key) {
+      }
+
+    uint64_t operator*() const {
+      return entry->value;
+    }
+
+    RangeIterator& operator++() {
+      do {
+        entry = entry->next;
+      } while (entry != nullptr && entry->key != key);
+      return *this;
+    }
+
+    RangeIterator operator++(int) {
+      RangeIterator r(*this);
+      operator++();
+      return r;
+    }
+
+    bool operator==(const RangeIterator& rhs) const {
+      return entry == rhs.entry && key == rhs.key;
+    }
+
+    bool operator!=(const RangeIterator& rhs) const {
+      return !operator==(rhs);
+    }
+
+  private:
+    Entry* entry;
+    uint64_t key;
 };
 
 //represents the results of a lookup
 class Range {
-
-public:
-  Range(Entry* chainStart, uint64_t key) :
+  public:
+    Range(Entry* chainStart, uint64_t key) :
       chainStart(chainStart), key(key) {
-  }
+      }
 
-  RangeIterator begin() {
-    return RangeIterator(chainStart, key);
-  }
+    RangeIterator begin() {
+      return RangeIterator(chainStart, key);
+    }
 
-  RangeIterator end() {
-    return RangeIterator(nullptr, key);
-  }
+    RangeIterator end() {
+      return RangeIterator(nullptr, key);
+    }
 
-private:
-  Entry* chainStart;
-  uint64_t key;
+  private:
+    Entry* chainStart;
+    uint64_t key;
 };
 
 #endif
